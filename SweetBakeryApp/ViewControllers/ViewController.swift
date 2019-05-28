58271e20e900ad52a65e43a285b19701ce9d980d
Anyloib <anyloib@gmail.com>
2019-05-28 11:24:21 -0700

    App Actualizada

---
+++ SweetBakeryApp/ViewControllers/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  SweetBakeryApp
+//
+//  Created by Alumno on 23/05/19.
+//  Copyright © 2019 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor(red: 229, green: 199, blue: 171, alpha: 1)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        self.view.backgroundColor = UIColor(red: 229, green: 199, blue: 171, alpha: 1)
+    }
+
+
+}
+
